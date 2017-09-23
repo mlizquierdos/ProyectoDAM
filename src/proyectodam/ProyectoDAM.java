3a095a835f6ccffa56ebfa1a0bf71d40218143cc
mlizquierdos <>
2017-09-23 08:33:45 +0200

    Hola mundo

--- src/proyectodam/ProyectoDAM.java
+++ src/proyectodam/ProyectoDAM.java
@@ -17,6 +17,7 @@ public class ProyectoDAM {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Esto sale en pantalla\n");
+        System.out.println("Versión 1.0\n");
     }
     
 }
